510fca63483347d20e5419efac24b1635350ed82
Robert Kaussow <mail@thegeeklab.de>
2023-07-21 00:25:45 +0200

    fix: fallback to stdin automatically if no url provided by flag

--- config/config.go
+++ config/config.go
@@ -3,11 +3,9 @@ package config
 import "errors"
 
 var (
-	ErrRequiredFlagsNotSet = errors.New("either \"url\" or \"stdin\" must be set")
-	ErrExclusiveFlags      = errors.New("\"url\" and \"stdin\" are mutually exclusive")
-	ErrEmptyStdin          = errors.New("\"stdin\" must not be empty")
-	ErrReadStdin           = errors.New("failed to read \"stdin\"")
-	ErrParseURL            = errors.New("failed to parse url")
+	ErrEmptyURL  = errors.New("no url provided either by \"url\" or \"stdin\"")
+	ErrReadStdin = errors.New("failed to read \"stdin\"")
+	ErrParseURL  = errors.New("failed to parse url")
 )
 
 type Config struct {
